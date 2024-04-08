8a6dcfa497ddea4e36740e1e66e899dd71f2e21d
osmanjosue <osmanjosue@gmail.com>
2024-04-08 12:24:46 -0600

    Seccion 9 => Exponer metodos del Hook

--- src/hooks/useCounter.js
+++ src/hooks/useCounter.js
@@ -4,8 +4,22 @@ export const useCounter = (initialValue = 10) => {
 
     const [counter, setCounter] = useState(initialValue);
 
+    const increment = (value = 1) => {
+        setCounter(counter + value);
+    }
+    const decrement = (value = 1) => {
+        // if (counter === 0) return;
+        setCounter(counter - value);
+    }
+    const reset = () => {
+        setCounter(initialValue);
+    }
+
     return {
-        counter
+        counter,
+        increment,
+        decrement,
+        reset
     }
 
 }
