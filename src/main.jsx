2981164906282aeba1545ee06e6132285b062167
osmanjosue <osmanjosue@gmail.com>
2024-04-08 11:29:01 -0600

    Seccion 9 => Clase: useState

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './hooks.App'
+import { CounterApp } from './01-useState/CounterApp'
+
+// import { HooksApp } from './hooks.App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HooksApp />
+    <CounterApp/>
   </React.StrictMode>,
 )
