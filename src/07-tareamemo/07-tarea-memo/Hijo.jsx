c28e69eb78be70137521ed9d25ac26aac3776035
osmanjosue <osmanjosue@gmail.com>
2024-04-10 17:34:04 -0600

    Seccion 9 => Parte#2 => Clase: Tarea Memorize

--- src/07-tareamemo/07-tarea-memo/Hijo.jsx
+++ src/07-tareamemo/07-tarea-memo/Hijo.jsx
@@ -1,15 +1,17 @@
-import React from 'react'
+import { memo } from "react";
 
-export const Hijo = ({ numero, incrementar }) => {
+export const Hijo = memo(
+    ({ numero, incrementar }) => {
 
-    console.log('  Me volví a generar :(  ');
-
-    return (
-        <button
-            className="btn btn-primary mr-3"
-            onClick={ () => incrementar( numero ) }
-        >
-            { numero }
-        </button>
-    )
-}
+        console.log('  Me volví a generar :(  ');
+    
+        return (
+            <button
+                className="btn btn-primary mr-3"
+                onClick={ () => incrementar( numero ) }
+            >
+                { numero }
+            </button>
+        )
+    }
+)
--- src/07-tareamemo/07-tarea-memo/Padre.jsx
+++ src/07-tareamemo/07-tarea-memo/Padre.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
 import { Hijo } from './Hijo'
-import { useState } from 'react';
+import { useCallback, useState } from 'react';
 
 export const Padre = () => {
 
     const numeros = [2,4,6,8,10];
     const [valor, setValor] = useState(0);
 
-    const incrementar = ( num ) => {
-        setValor( valor + num )
-    }
+    const incrementar = useCallback(
+        ( num ) => {
+            setValor( (value)=> value + num )
+        },[],
+    )
 
 
     return (
