513cb09e172f9846752326b64b1ce9757358e981
osmanjosue <osmanjosue@gmail.com>
2024-04-15 11:44:05 -0600

    Seccion 11 => Clase: configurar Router en React

--- src/09-useContext/MainApp.jsx
+++ src/09-useContext/MainApp.jsx
@@ -1,8 +1,37 @@
+import { HomePage, AboutPage, LoginPage } from './index';
+import { Navigate, Route, Routes } from 'react-router-dom';
+/* 
+import { AboutPage } from './AboutPage';
+import { HomePage } from './HomePage';
+import { LoginPage } from './LoginPage'; */
+
 export const MainApp = () => {
   return (
     <>
       <h1>MainApp</h1>
       <hr />
+      <Routes>
+        <Route
+          path="/"
+          element={<HomePage />}
+        />
+        <Route
+          path="about"
+          element={<AboutPage />}
+        />
+        <Route
+          path="login"
+          element={<LoginPage />}
+        />
+        {/* <Route
+          path="/*"
+          element={<LoginPage />}
+        /> */}
+        <Route
+          path="/*"
+          element={<Navigate to="/login" />}
+        />
+      </Routes>
     </>
   );
 };
