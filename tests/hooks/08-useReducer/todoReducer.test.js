d872d55404d0ad1bacec9e519f903624d6fdfb9b
osmanjosue <osmanjosue@gmail.com>
2024-04-19 21:39:43 -0600

    Seccion 12 => Clases: Pruebas sobre el Reducer & Resolucion de la tarea

---
+++ tests/hooks/08-useReducer/todoReducer.test.js
@@ -0,0 +1,60 @@
+import { todoReducer } from "../../../src/08-useReducer/todoReducer";
+
+describe('Pruebas en todoReducer', () => {
+
+    const initialState = [{
+        id: 1,
+        description: 'Demo Todo',
+        done: false,
+    }];
+
+    test('should return the initial state', () => {
+
+        const newState = todoReducer(initialState, {});
+        expect(newState).toBe(initialState);
+
+    });
+
+    test('should add a Todo', () => {
+        const action = {
+            type: '[TODO] Add Todo',
+            payload: {
+                id: 2,
+                description: 'Nuevo todo #2',
+                done: false,
+            }
+        };
+
+        const newState = todoReducer(initialState, action);
+        expect(newState.length).toBe(2);
+        expect(newState).toContain(action.payload);
+
+    });
+
+    test('should delete a TODO', () => {
+
+        const action = {
+            type: '[TODO] Remove Todo',
+            payload: 1,
+        };
+
+        const newState = todoReducer(initialState, action);
+        expect(newState.length).toBe(0);
+
+    });
+
+    test('should make the TODO toggle', () => {
+
+        const action = {
+            type: '[TODO] Toggle Todo',
+            payload: 1,
+        };
+
+        const newState = todoReducer(initialState, action);
+        expect(newState[0].done).toBe(true);
+
+        const newState2 = todoReducer(newState, action);
+        expect(newState2[0].done).toBe(false);
+
+    })
+})
