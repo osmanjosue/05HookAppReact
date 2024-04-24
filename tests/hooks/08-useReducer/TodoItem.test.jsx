47ab1069c4ec16a7aebcb3b8ac5d0482308136e1
osmanjosue <osmanjosue@gmail.com>
2024-04-24 11:10:43 -0600

    Seccion 12 => Clase: Pruebas en los eventos del TodoItem

--- tests/hooks/08-useReducer/TodoItem.test.jsx
+++ tests/hooks/08-useReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import { TodoItem } from '../../../src/08-useReducer/TodoItem';
 
 describe('Pruebas en <TodoItem />', () => {
@@ -29,8 +29,55 @@ describe('Pruebas en <TodoItem />', () => {
 
     const spanElement = screen.getByLabelText('span');
     expect(spanElement.className).toContain('align-self-center');
-    expect( spanElement.className).not.toContain('text-decoration-line-through')
-    
+    expect(spanElement.className).not.toContain('text-decoration-line-through');
+
     // screen.debug();
+  });
+  test('should show the TODO completed', () => {
+    todo.done = true;
+
+    render(
+      <TodoItem
+        todoReceived={todo}
+        onDeleteTodo={onDeleteTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText('span');
+    expect(spanElement.className).toContain('text-decoration-line-through');
+
+    // screen.debug();
+  });
+  test('should call the ToggleTodo function when click is made', () => {
+    render(
+      <TodoItem
+        todoReceived={todo}
+        onDeleteTodo={onDeleteTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    );
+
+    const spanElement = screen.getByLabelText('span');
+
+    fireEvent.click(spanElement);
+
+    expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+  });
+
+  test('Button should call the deleteTodo', () => {
+    render(
+      <TodoItem
+        todoReceived={todo}
+        onDeleteTodo={onDeleteTodoMock}
+        onToggleTodo={onToggleTodoMock}
+      />
+    );
+
+    const buttonElement = screen.getByRole('button');
+
+    fireEvent.click(buttonElement);
+
+    expect(onDeleteTodoMock).toHaveBeenCalledWith(todo.id);
   });
 });
