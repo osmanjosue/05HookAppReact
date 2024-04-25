950f325619b67c7bd9d10d09fc3161909eb531ba
osmanjosue <osmanjosue@gmail.com>
2024-04-25 16:25:14 -0600

    Seccion 12 => Clase: Pruebas generales en nuestro AppRouter

---
+++ tests/09-useContext/MainApp.test.jsx
@@ -0,0 +1,22 @@
+import { render, screen } from '@testing-library/react';
+import { MainApp } from '../../src/09-useContext/MainApp';
+import { MemoryRouter } from 'react-router-dom';
+
+describe('Pruebas en <MainApp />', () => {
+  test('should show the HomePage', () => {
+    render(
+      <MemoryRouter>
+        <MainApp />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('HomePage')).toBeTruthy();
+  });
+  test('should show the HomePage', () => {
+    render(
+      <MemoryRouter initialEntries={['/login']}>
+        <MainApp />
+      </MemoryRouter>
+    );
+    expect(screen.getByText('LoginPage')).toBeTruthy();
+  });
+});
